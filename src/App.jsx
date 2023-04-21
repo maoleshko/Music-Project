67b9843717b1355023921632b9c4d178fcd2f724
Maoleshko <ranshe.rassveta@gmail.com>
2023-04-21 20:57:35 +0300

    5.2.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './style/App.css';
 import PlaylistItem from "./components/Playlist/PlaylistItem";
-import Search from './components/Search/search';
+import Search from './components/Search/Search';
 import Sidebar from './components/Sidebar/Sidebar'
 
 import Menu from './components/burgerMenu/Menu';
 import MainCenterblock from './components/centerblock/MainCenterblock';
+import BarPlayer from './components/BarPlayer/BarPlayer';
 
 function App() {
   const [menuActive, setMenuActive] = useState(false)
@@ -28,12 +29,18 @@ function App() {
             <PlaylistItem track={{title: 'I’m Fire',author: 'Ali Bakgor',album: 'I’m Fire', time:'2:22'}}/>
             <PlaylistItem track={{title: 'Non Stop',author: 'Стоункат, Psychopath',album: 'Non Stop', time:'4:412'}}/>
             <PlaylistItem track={{title: 'Run Run',author: 'Jaded, Will Clarke, AR/CO',album: 'Run Run', time:'2:54'}}/>
+            <PlaylistItem track={{title: 'Guilt',author: 'Nero',album: 'Welcome Reality', time:'4:44'}}/>
+            <PlaylistItem track={{title: 'Elektro',author: 'Dynoro, Outwork, Mr. Gee',album: 'Elektro', time:'2:22'}}/>
+            <PlaylistItem track={{title: 'I’m Fire',author: 'Ali Bakgor',album: 'I’m Fire', time:'2:22'}}/>
+            <PlaylistItem track={{title: 'Non Stop',author: 'Стоункат, Psychopath',album: 'Non Stop', time:'4:412'}}/>
+            <PlaylistItem track={{title: 'Run Run',author: 'Jaded, Will Clarke, AR/CO',album: 'Run Run', time:'2:54'}}/>
           </div>
         </div>
         <div>
           <Sidebar/>
           </div>
       </main>
+      <div><BarPlayer/></div>
       <Menu active={menuActive} setActive={setMenuActive} header={""} item={item}/>
    
     
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Sidebar.css';
+import playlist01 from '../../img/playlist01.png'
+import playlist02 from '../../img/playlist02.png'
+import playlist03 from '../../img/playlist03.png'
 
 
 function Sidebar() {
@@ -8,17 +11,17 @@ function Sidebar() {
         <div className="sidebar__list">
             <div className="sidebar__item">
                 <a className="sidebar__link" href="#">
-                    <img className="sidebar__img" src="img/playlist01.png" alt="day's playlist"/>
+                    <img className="sidebar__img" src={playlist01} alt="day's playlist"/>
                 </a>
             </div>
             <div className="sidebar__item">
                 <a className="sidebar__link" href="#">
-                    <img className="sidebar__img" src="img/playlist02.png" alt="day's playlist"/>
+                <img className="sidebar__img" src={playlist02} alt="100 dance hit"/>
                 </a>
             </div>
             <div className="sidebar__item">
                 <a className="sidebar__link" href="#">
-                    <img className="sidebar__img" src="img/playlist03.png" alt="day's playlist"/>
+                    <img className="sidebar__img" src={playlist03} alt="indi-energy"/>
                 </a>
             </div>
         </div>
