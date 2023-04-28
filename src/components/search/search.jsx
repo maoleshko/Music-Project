d1191eaa1a87a6532a6adbc984272c5553326cb9
Maoleshko <63302289+maoleshko@users.noreply.github.com>
2023-04-28 12:14:20 +0300

    Add files via upload

---
+++ src/components/search/search.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './search.css';
+import sprite from '../../img/icon/sprite.svg'
+
+function Search() {
+  return (
+    <div className="centerblock__search search">
+      <svg className="search__svg">
+        <use xlinkHref={`${sprite}#icon-search`}></use>
+      </svg>
+      <input className="search__text" type="search" placeholder="Поиск" name="search"/>
+    </div>
+    )
+}
+
+export default  Search;
